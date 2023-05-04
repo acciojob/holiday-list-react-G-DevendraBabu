614625fee99b13cd68ab6d0c1cb4707e1013a1bc
G-DevendraBabu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-05-04 23:23:08 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,35 @@
 
-import React, { useState, useEffect } from "react";
-import './../styles/App.css';
+function uniqueCategories(arr) {
+	let ans = [];
+	let len = arr.length;
+	for(let i=0; i<len; i++){
+		if(!ans.includes(arr[i][1])){
+			ans.push(arr[i][1]);
+		}
+	}
+	return ans;
+}
+var readline = require("readline").createInterface(process.stdin);
+let inputArr = [];
+var lineNumber = -1;
 
-const App = () => {
-    const cityList = [{ name: 'Goa', country: 'India' },
-    { name: 'Amsterdam', country: 'Netherlands' },
-    { name: 'New York', country: 'USA' },
-    { name: 'Darjeeling', country: 'India' },
-    { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
-    ]
-    
-  return (
-    <div id="main">
-               {/* Do not remove the main div */}
-    </div>
-  )
+readline.on("line", readInputs);
+
+function readInputs(line) {
+  inputArr.push(line);
+  lineNumber++;
+ 
+  if (lineNumber ==  0) {
+    logic("s");
+    readline.close();
+  }
 }
 
-export default App
+
+
+function logic(input) {
+    let arr=JSON.parse(inputArr[0]);
+    let word=inputArr[1];
+    console.log(uniqueCategories(arr).join(" "));
+    
+}
